2a3c5a135279de8f096636f4f060866507bf952d
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-12-28 01:23:57 -0300

    Initial commit

--- src/padding.jl
+++ src/padding.jl
@@ -1,13 +1,11 @@
 add_padding(data::AbstractString, args...) = add_padding(Vector{UInt8}(data), args...)
 
-add_padding(data::Vector{UInt32}, args...) = add_padding(to_vector_uint8(data), args...)
+# add_padding(data::Vector{UInt32}, args...) = add_padding(to_vector_uint8(data), args...)
 
 function add_padding(data::Vector{UInt8}, block_size::Integer = 16)
     size = block_size - (length(data) % block_size)
     return vcat(data, [UInt8(size) for _ in 1:size])
 end
-
-remove_padding(data::AbstractString) = remove_padding(Vector{UInt8}(data))
 
 remove_padding(data::Vector{UInt32}) = remove_padding(to_vector_uint8(data))
 
