72a4532eca770cddfdda72d25602095ad4d7620c
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-17 00:01:45 -0300

    Update

--- test/test_sub_word.jl
+++ test/test_sub_word.jl
@@ -4,7 +4,7 @@ using Test
 
 import AdvancedEncryptionStandard as AES
 
-@testset "SubWord" begin
+@testset "Sub Word" begin
     a = [0x8e9ff1c6, 0x4ddce1c7, 0xa158d1c8, 0xbc9dc1c9]
     b = [0x19dba1b4, 0xe386f8c6, 0x326a3ee8, 0x655e78dd]
 
